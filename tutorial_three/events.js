8984d8fc50df1b4c231b366a9159a1e18c35a8a4
ammarhussien <ammar.hussienmo97@gmail.com>
2024-03-05 20:54:45 +0200

    new

---
+++ tutorial_three/events.js
@@ -0,0 +1,17 @@
+// Basic Data types & operators
+
+var text = "Hello, world!";  // string
+
+var number = 2024; // number
+
+var isBoolean = true; // boolean (true , false)
+
+var hello // undefined
+
+var nothing = null // Null
+
+var array = ['hat', 'orange', 'car'] // array
+
+var object = { name: "Ammar", age: 27 } // object
+
+//console.log(typeof(object))
